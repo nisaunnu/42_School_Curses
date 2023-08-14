396d465e75d8c40035d9f18467cdb8e05911799e
Nisa Ünnü <65195841+nisaunnu@users.noreply.github.com>
2023-08-14 14:08:06 +0300

    Added libft_main .c files

---
+++ libft_main/ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nunnu <nunnu@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 05:42:36 by nunnu             #+#    #+#             */
+/*   Updated: 2023/07/23 21:39:57 by nunnu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	result = 0;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+	{
+		sign *= -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	if (*str == '-' || *str == '+')
+		return (0);
+	while (ft_isdigit(*str) == 1)
+	{
+		result = (result * 10) + (*str - '0');
+		str++;
+	}
+	return (result * sign);
+}
+
+// #include <stdio.h>
+
+// int main()
+// {
+// 	printf("Output: %d\n", ft_atoi("  +-14031879"));
+// 	printf("Output: %d\n", ft_atoi("   +14031879"));
+// }
