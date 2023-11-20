a957cd8d110d30ffc0c3a64cbe847c1cdc385470
Nisa Ünnü <65195841+nisaunnu@users.noreply.github.com>
2023-11-21 02:12:18 +0300

    rot_13

--- Level 0 - rot_13/rot_13.c
+++ Level 0 - rot_13/rot_13.c
@@ -2,18 +2,16 @@
 
 int	main(int ac, char **av)
 {
-	int i = 0;
-
 	if (ac == 2)
 	{
-		while(av[1][i])
+		while(*av[1])
 		{
-			if ((av[1][i] >= 'a' && av[1][i] <= 'm') || (av[1][i] >= 'A' && av[1][i] <= 'M'))
-				av[1][i] += 13;
-			else if ((av[1][i] >= 'n' && av[1][i] <= 'z') || (av[1][i] >= 'N' && av[1][i] <= 'Z'))
-				av[1][i] -= 13;
-			write(1, &av[1][i], 1);
-			i++;
+			if (('a' <= *av[1] && *av[1] <= 'm') || ('A' <= *av[1] && *av[1] <= 'M'))
+				*av[1] += 13;
+			else if (('n' <= *av[1] && *av[1] <= 'z') || ('N' <= *av[1] && *av[1] <= 'Z'))
+				*av[1] -= 13;
+			write(1, av[1], 1);
+			av[1]++;
 		}
 	}
 	write(1, "\n", 1);
