dbaa04e1834d903d28b6fce8ae0b0126026931b3
Nisa Ünnü <65195841+nisaunnu@users.noreply.github.com>
2024-03-03 16:18:33 +0300

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,107 +1,103 @@
 #include "get_next_line.h"
 
-int ft_strlen(char *str)
+int	ft_strlen(char *s)
 {
 	int i = 0;
-	while (str && str[i])
+	while (s && s[i])
 		i++;
-	return i;
-}
-
-char *ft_strjoin(char *str1, char *str2)
-{
-	char *tmp = malloc(ft_strlen(str1) + ft_strlen(str2) + 1 );
-	int i = 0;
-	while (str1 && str1[i])
-	{
-		tmp[i] = str1[i];
-		i++;
-	}
-	int j = 0;
-	while(str2 && str2[j])
-	{
-		tmp[i + j] = str2[j];
-		j++;
-	}
-	tmp[i + j] = '\0';
-	return(tmp);
+	return (i);
 }
 
 char *ft_substr(char *str, int i)
 {
-	char *tmp = malloc(i + 1);
+	char *s = malloc(i + 1);
 	int j = 0;
-	while (str[i] && j < i)
+	while (str[j] && j < i)
 	{
-		tmp[j] = str[j];
+		s[j] = str[j];
 		j++;
 	}
-	tmp[j] = '\0';
-	return(tmp);
+	s[j] = '\0';
+	return (s);
 }
 
-int check(char *line)
+int	check(char *line)
 {
-	if(!line)
-		return -1;
+	if (!line)
+		return (-1);
 	int i = 0;
 	while (line[i])
 	{
-		if(line[i] == '\n')
-			return i;
+		if (line[i] == '\n')
+			return (i);
 		i++;
 	}
-	return -1;
+	return (-1);
+}
+
+char *ft_strjoin(char *s1, char *s2)
+{
+	char *s = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	int i = 0;
+	int j = 0;
+	while (s1 && s1[i])
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	while (s2 && s2[j])
+	{
+		s[i + j] = s2[j];
+		j++;
+	}
+	s[i + j] = '\0';
+	return (s);
 }
 
 char *next_line(char *line, int fd)
 {
-	int i = 1;
-	char *tmp;
-	while (check(line) < 0 && i != 0)
+	char *s;
+	int bytes_read = 1;
+	while (bytes_read != 0 && check(line) < 0)
 	{
-		tmp = malloc(BUFFER_SIZE + 1);
-		i = read(fd, tmp, BUFFER_SIZE);
-		if(i < 0)
-		{
-			free(line);
-			free(tmp);
-			return NULL;
-		}
-		if(i == 0 && !line)
-			return(free(tmp), NULL);
-		tmp[i] = '\0';
-		line = ft_strjoin(line, tmp);
-		free(tmp);
+		s = malloc(BUFFER_SIZE + 1);
+		bytes_read = read(fd, s, BUFFER_SIZE);
+		if (bytes_read < 0 )
+			return (free (line), free(s), NULL);
+		if (bytes_read == 0 && !line)
+			return (free(s), NULL);
+		s[bytes_read] = '\0';
+		line = ft_strjoin(line, s);
+		free (s);
 	}
 	return (line);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	if(BUFFER_SIZE <= 0 || fd < 0)
-		return NULL;
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
 	static char *line = NULL;
-	line = next_line(line, fd);
 	char *str;
+	line = next_line(line, fd);
 	int i = check(line);
 	char *tmp;
-	if(line != NULL)
+	if (line != NULL)
 	{
-		if(i != -1)
+		if (i != -1)
 		{
 			tmp = line;
 			str = ft_substr(line, i + 1);
 			line = ft_substr(&line[i + 1], ft_strlen(line) - i);
-			free(tmp);
+			free (tmp);
 		}
 		else
 		{
 			str = ft_substr(line, ft_strlen(line));
+			free (line);
 			line = NULL;
-			free(line);
 		}
-	return (str);
+		return (str);
 	}
-	return NULL;
+	return (NULL);
 }
